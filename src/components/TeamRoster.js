44bef41e6cd077e8306ad94ce637f6381321e219
Cynthiad215 <Cynthiad215@users.noreply.github.com>
2020-10-14 21:01:26 -0700

    updates to TeamRoster component

--- src/components/TeamRoster.js
+++ src/components/TeamRoster.js
@@ -2,33 +2,51 @@ import React, {useEffect, useState} from 'react';
 import PlayerCard from './PlayerCard';
 
 const TeamRoster = (props) => {
-    console.log('in team roster', props)
-    //const [playerNames, setPlayerNames] = useState([]);
-    const [playerBios, setPlayerBios]     = useState([])
+   
+    const [playerNames, setPlayerNames] = useState([]);
+    const [searchPlayer, setSearchPlayer] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+    const [playerBios, setPlayerBios] = useState([]);
+    const [playerBiosTemp, setPlayerBiosTemp] = useState([]);
+
+    const handleInput = (e) => {
+        setSearchPlayer(e.target.value);
+        let tempArr = [];
+        tempArr = playerNames.filter((key, value) => {
+            return key.startsWith(e.target.value);
+        });
+        if (e.target.value == "") {
+            setSearchResult("");
+        } else {
+            setSearchResult(tempArr);
+        }
+
+        console.log('pbt', playerBiosTemp)
+    }
     useEffect(() => {
         async function getRoster(){
             await fetch(`http://localhost:8080/teams/roster/${props.match.params.teamId}`)
                 .then(response=>response.json()
-                    //console.log(response.json());
                 ).then((roster)=>{
-                    
-                   /* let playerNames = []
-                    roster.forEach((key, value) => {
-                        playerNames.push(key.person.fullName)
-                        
-                    });
-                    setPlayerNames(playerNames);
-                    */
+                    let playerNamesArr =[];
                     let playerBiosArr = [];
+                    let playerBiosTempArr =[];
                     roster.forEach((key, value) => {
+
+                        let tempID = key.person.id;
+
+                        let name = key.person.fullName;
+                        //console.log(name)
+                        playerBiosTempArr.push({tempID, name});
+
+                        playerNamesArr.push(key.person.fullName);
                         playerBiosArr.push(key);
                     });
-                    playerBiosArr.sort();
                     setPlayerBios(playerBiosArr);
-                    //console.log(playerBiosArr)
+                    setPlayerNames(playerNamesArr);
+                    setPlayerBiosTemp(playerBiosTempArr);
                 });
         };
-
         getRoster();
 
     }, [])
@@ -37,7 +55,14 @@ const TeamRoster = (props) => {
     return (
         <div className="container pt-5 ">
             <h1 className="text-center">{props.location.state.team}</h1>
+            <input 
+                value={searchPlayer}
+                onChange={handleInput}
+                type="text"
+                />
+                <span>{searchResult}</span>
             <div className="player-card-list text-center">
+            
             {playerBios ? playerBios.map((key,value) => {
                 return <PlayerCard 
                             key={`${value}`} 
