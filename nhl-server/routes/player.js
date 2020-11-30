6331df92f911596241216066573ff82cc43986d4
Cynthiad215 <Cynthiad215@users.noreply.github.com>
2020-11-30 13:08:44 -0800

    update player age info

--- nhl-server/routes/player.js
+++ nhl-server/routes/player.js
@@ -3,20 +3,24 @@ const router = express.Router();
 var got = require('got');
 var cors = require('cors');
 
-router.get('/:id', cors(), function(req, res){
+router.get('/:id', cors(), function (req, res) {
     (async () => {
         try {
-            const response = await got('https://statsapi.web.nhl.com/api/v1/people/' + req.params.id + '?expand=person.stats&stats=yearByYear,yearByYearPlayoffs,careerRegularSeason&expand=stats.team');
-            
+            let d = new Date();
+            let thisYear = d.getFullYear();
+            let lastYear = d.getFullYear() - 1;
+            let yearRange = lastYear.toString() + thisYear.toString();
+
+            //const response = await got('https://statsapi.web.nhl.com/api/v1/people/' + req.params.id + '?expand=person.stats&stats=yearByYear,yearByYearPlayoffs,careerRegularSeason&expand=stats.team');
+            const response = await got('https://statsapi.web.nhl.com/api/v1/people/' + req.params.id + '?expand=person.stats&stats=statsSingleSeason&season=' + yearRange);
             const result = JSON.parse(response.body);
-            //pass relevant league season info
             res.status(200).send(result.people);
         } catch (error) {
-            res.status(400).send({"Error": error, "Message":'Error with grabbing Player info'})
+            res.status(400).send({"Error": error, "Message": 'Error with grabbing Player info'})
 
 
-        } 
-        
+        }
+
     })();
 });
 module.exports = router;
